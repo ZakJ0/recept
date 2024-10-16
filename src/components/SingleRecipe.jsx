a502ba667130c15e4a115df9642295ecbc1ef825
SorellaTavares <145568553+SorellaTavares@users.noreply.github.com>
2024-10-16 21:43:34 +0200

    Merged with featuremMERGE

--- src/components/SingleRecipe.jsx
+++ src/components/SingleRecipe.jsx
@@ -13,18 +13,3 @@ function SingleRecipe() {
 }
 
 export default SingleRecipe;
-import React from "react";
-import { useParams } from "react-router-dom";
-import headerImage from '../assets/images/headdish.jpg';
-
-function SingleRecipe() {
-    const { id } = useParams();
-    return (
-        <div>
-            <img src={headerImage} id="headerimg" alt="tasty dish image" />
-            <h1>Food galaxy</h1>
-        </div>
-    );
-}
-
-export default SingleRecipe;
