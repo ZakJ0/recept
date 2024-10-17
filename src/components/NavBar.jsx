5da5d9185bbdd6280d05a0a3790bf409e00f52bf
ZakJ0 <zakaria.jaouhari89@gmail.com>
2024-10-17 12:01:05 +0200

    knas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <ul className="nav-links">
-                <li><Link to="/recipe-list">Kategori 1</Link></li>
+                <li><Link to="/recipe-list">Homepage</Link></li>
                 <li><a href="#2">Kategori 2</a></li>
                 <li><a href="#3">Kategori 3</a></li>
             </ul>
